76b65007b9a362407b61a3eb2bc52711f7bb2530
Abdullah Amir <abdullahcrest&@gmail.com>
2025-07-08 00:42:17 +0500

    Pehla project add kia

---
+++ bolo.py
@@ -0,0 +1,9 @@
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("fuck you haris raza")
+# engine.runAndWait()
+# print("hello world")
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("abd champ")
+engine.runAndWait()
