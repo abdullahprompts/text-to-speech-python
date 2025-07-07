4dcf7f585d832b04bd4ecf4ec8957d7334737bbe
Abdullah Amir <abdullahprompts@gmail.com>
2025-07-08 02:33:14 +0500

    Delete bolo.py

--- bolo.py
+++
@@ -1,9 +0,0 @@
-# import pyttsx3
-# engine = pyttsx3.init()
-# engine.say("fuck you haris raza")
-# engine.runAndWait()
-# print("hello world")
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("abd champ")
-engine.runAndWait()
